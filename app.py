8a1236d0eb7363e3f94877257cd1ef31a214e814
Miguel Vila <miguel.vila.dev@gmail.com>
2020-09-25 10:42:22 +0100

    Implemented functionality.

--- app.py
+++ app.py
@@ -1,63 +1,40 @@
 import urllib.request
+import os
 from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPDF
+from reportlab.graphics import renderPDF as pdf_render
 from PyPDF2 import PdfFileMerger
 
-base_url = 'https://www.escolavirtual.pt/emanuais-cs/[isbn]-SE-02/html5/[isbn]-SE-02-lite//OPS/images/page[current_page].svgz'
-#https://www.escolavirtual.pt/emanuais-cs/9789720856463-SE-01/html5/9789720856463-SE-01-lite//OPS/images/page0010.svgz
-isbn = '9789720856463'
+emanual_base_url = 'https://www.escolavirtual.pt/emanuais-cs/[isbn]-SE-[part]/html5/[isbn]-SE-[part]-lite//OPS/images/page[current_page].svgz'
+isbn = '978-972-0-85336-3'
+part = 2
 current_page = 1
-current_url= ''
-pdf_collection = []
 
-base_url = base_url.replace('[isbn]', isbn)
 
+emanual_base_url = (emanual_base_url.replace('[isbn]', isbn.replace('-',''))).replace('[part]','{:02d}'.format(part))
+pdf_merger = PdfFileMerger()
 while True:
     try:
-        current_url = base_url.replace('[current_page]','{:04d}'.format(current_page))
+        print('[INFO] Getting page number {:04d}'.format(current_page))
         temp_svg = open('temp.svg','w')
-        web_svgz = urllib.request.urlopen(current_url)
+        web_svgz = urllib.request.urlopen(emanual_base_url.replace('[current_page]','{:04d}'.format(current_page)))
         temp_svg.write(web_svgz.read().decode("utf8"))
         web_svgz.close()
         temp_svg.close()
         pdf_drawer = svg2rlg("temp.svg")
-        renderPDF.drawToFile(pdf_drawer, 'page{:04d}.pdf'.format(current_page))
-        pdf_collection.append('page{:04d}.pdf'.format(current_page))
+        pdf_render.drawToFile(pdf_drawer, 'temp{:04d}.pdf'.format(current_page))
+        pdf_merger.append('temp{:04d}.pdf'.format(current_page))
         current_page += 1
     except:
+        if current_page == 1:
+            print('[ERROR] The e-manual does not exist. Please check ISBN and PART number.')
+        else:
+            print('[INFO] Finnished downloading all pages.')
+            print('[INFO] Merging pdf files.')
+            final_pdf = open("E-Manual.pdf", "wb")
+            pdf_merger.write(final_pdf)
+            pdf_merger.close()
         break
-
-merger = PdfFileMerger()
-for pdf in pdf_collection:
-    merger.append(pdf)
-merger.write("result.pdf")
-merger.close() 
-
-"""
-f= open("page.svg","w")
-fp = urllib.request.urlopen("https://www.escolavirtual.pt/emanuais-cs/9789720850188-SE-01/html5/9789720850188-SE-01-lite//OPS/images/page0320.svgz")
-f.write(fp.read().decode("utf8"))
-fp.close()
-f.close()
-drawing = svg2rlg("./page.svg")
-renderPDF.drawToFile(drawing, "file1.pdf")
-
-f= open("page.svg","w")
-fp = urllib.request.urlopen("https://www.escolavirtual.pt/emanuais-cs/9789720850188-SE-01/html5/9789720850188-SE-01-lite//OPS/images/page0321.svgz")
-f.write(fp.read().decode("utf8"))
-fp.close()
-f.close()
-drawing = svg2rlg("./page.svg")
-renderPDF.drawToFile(drawing, "file2.pdf")
-
-
-pdfs = ['file1.pdf', 'file2.pdf']
-
-merger = PdfFileMerger()
-
-for pdf in pdfs:
-    merger.append(pdf)
-
-merger.write("result.pdf")
-merger.close()
-"""
+print('[INFO] Clearing temporary files.')
+for temp_file in os.listdir('.'):
+    if ('temp' in temp_file) and (('.svg' in temp_file) or ('.pdf' in temp_file)):
+        os.remove(temp_file) 
