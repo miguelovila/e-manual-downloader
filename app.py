3a2b917522a367be7e70cd2ddc65890af84d2503
Miguel Vila <miguel.vila.dev@gmail.com>
2020-09-28 13:45:24 +0100

    Minor code changes.

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from PyPDF2 import PdfFileMerger
 
 emanual_base_url_student = 'https://www.escolavirtual.pt/emanuais-cs/[isbn]-SE-[part]/html5/[isbn]-SE-[part]-lite//OPS/images/page[current_page].svgz'
 emanual_base_url_teacher = 'https://www.escolavirtual.pt/emanuais-cs/[isbn]-TE-[part]/html5/[isbn]-TE-[part]-lite//OPS/images/page[current_page].svgz'
-isbn = '978-972-0-85336-3'
-part = 2
+
+isbn = '9789720856296'
+part = 1
 current_page = 1
 
-#emanual_base_url = (emanual_base_url_teacher.replace('[isbn]', isbn.replace('-',''))).replace('[part]','{:02d}'.format(part))
-emanual_base_url = (emanual_base_url_student.replace('[isbn]', isbn.replace('-',''))).replace('[part]','{:02d}'.format(part))
+emanual_base_url = (emanual_base_url_teacher.replace('[isbn]', isbn.replace('-',''))).replace('[part]','{:02d}'.format(part))
+#emanual_base_url = (emanual_base_url_student.replace('[isbn]', isbn.replace('-',''))).replace('[part]','{:02d}'.format(part))
 
 pdf_merger = PdfFileMerger()
 while True:
